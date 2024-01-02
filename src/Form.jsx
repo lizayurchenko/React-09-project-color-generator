709e0425499350d42b6b43329f63461db886e254
Yelyzaveta Yurchenko <yelyzavetayurchenko@Noutbuk-Yelyzaveta.local>
2024-01-02 19:00:33 +0100

    first commit

---
+++ src/Form.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const Form = ({ addColor }) => {
+  const [color, setColor] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addColor(color);
+  };
+
+  return (
+    <section className="container">
+      <h4>color generator</h4>
+      <form className="color-form" onSubmit={handleSubmit}>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="#315c72"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <button className="btn" style={{ background: color }}>
+          submit
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
